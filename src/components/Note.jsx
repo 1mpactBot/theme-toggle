36b565ccbe20d6942c2be264fc294caefbaa53f5
1mpactBot <64764777+1mpactBot@users.noreply.github.com>
2020-11-21 21:14:09 +0530

    Update Note.jsx

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -4,7 +4,7 @@ import ThemeContext,{useTheme} from "./ThemeContext";
 function Note() {
 const theme=useTheme();
   return (
-    <div className="note" style={theme} >
+    <div className="note" style={{backgroundColor:theme.cardBG , color: theme.color }} >
       <h1>Heading</h1>
       <p>Content</p>
     </div>
