f412d39967a254f328b56b94baf890a6ed98c71f
1mpactBot <64764777+1mpactBot@users.noreply.github.com>
2020-11-21 21:16:20 +0530

    Update ThemeContext.js

--- src/components/ThemeContext.js
+++ src/components/ThemeContext.js
@@ -4,10 +4,14 @@ export const themes={
   lightGlobal:{
     color:"#000",
     backgroundColor:"#f1f3f8",
+    cardBG: "#f1f3f8",
+    headerBG: "#9d65c9"
   },
   darkGlobal:{
     color:"#f1f3f8",
-    backgroundColor:"#30475e",
+    backgroundColor:"#1a1a2e",
+    cardBG: "#0f3460",
+    headerBG: "#1a1a2e"
   }
 };
 export const useTheme = () => useContext(ThemeContext);
